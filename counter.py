d7f8a63ddb7f3c75397a36a1618f904350c9f6b3
Yinnohs <joistleon@gmail.com>
2022-05-30 12:48:37 +0100

    test: passing_test_2

--- counter.py
+++ counter.py
@@ -1,13 +1,28 @@
 
 class MyCounter:
 
-    def __init__(self, startValue = 0, incrementalValue, limitValue ):
-        self.startValue_ = startValue
-        self.incrementalValue_ = incrementalValue
-        self.limitValue_ = limitValue
+    def __init__(self, startValue = None, incrementalValue = None, limitValue = None ):
+        if startValue is None:
+            self.startValue_ = 0;
+        else:
+            self.startValue_ = startValue
+
+
+        if incrementalValue is None:
+            self.incrementalValue_ = 1
+        else:
+            self.incrementalValue_ = incrementalValue
+
+
+        if limitValue is None:
+            print ("cannot be empty")
+        else: 
+            self.limitValue_ = limitValue
     
     def count(self):
-        self.startValue_ += self.incrementalValue_
-    
+        if(self.startValue_< self.limitValue_):
+            self.startValue_ += self.incrementalValue_
+        else:
+            print(" the current value is bigger than the limit value ")
     def getCurrentNumber(self):
         return self.startValue_
