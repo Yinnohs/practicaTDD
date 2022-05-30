5076083199198efc626bb9bf911d946c4e0f2a0d
Yinnohs <joistleon@gmail.com>
2022-05-30 13:03:52 +0100

    feat: countercount function

--- counter.py
+++ counter.py
@@ -1,12 +1,12 @@
 
 class MyCounter:
-
+    currentValue_= 0
     def __init__(self, startValue = None, incrementalValue = None, limitValue = None ):
         if startValue is None:
             self.startValue_ = 0;
         else:
             self.startValue_ = startValue
-
+            self.currentValue_ = startValue
 
         if incrementalValue is None:
             self.incrementalValue_ = 1
@@ -20,9 +20,11 @@ class MyCounter:
             self.limitValue_ = limitValue
     
     def count(self):
-        if(self.startValue_< self.limitValue_):
-            self.startValue_ += self.incrementalValue_
+        if(self.currentValue_< self.limitValue_):
+            self.currentValue_ += self.incrementalValue_
         else:
             print(" the current value is bigger than the limit value ")
+            self.currentValue_= self.startValue_
+
     def getCurrentNumber(self):
-        return self.startValue_
+        return self.currentValue_
